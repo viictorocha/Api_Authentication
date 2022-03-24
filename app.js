fd15cc300b46646d2306f7e8130dcbfac873a356
10viictor39 <84255334+10viictor39@users.noreply.github.com>
2022-03-24 13:04:22 -0300

    validando rotas

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const security = require('./security.js')
 
-http.createServer((req, res) => {
-  const { url , method} = server;
+//const rotaSistema  = require('./routes/sistema');
+//const rotaUsers    = require('./routes/user');
+//const rotaLogin    = require('./routes/login');
+//const rotaData     = require('./routes/data');
+//const rotaClient   = require('./routes/client');
 
-  if(url === '/' && method === 'GET') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>teste<h1>'); //write a response
-    res.end(); //end the response  
-  }
+app.use(bodyParser.urlencoded({ extended: false })); //Aceita apenas dados simples
+app.use(bodyParser.json());
 
-  res.writeHead(404, {'Content-Type': 'text/html'});
-  res.write("Error, the URL doesn't exist");
-  res.end(); //end the response  
+app.use(express.static("public"))
 
-  
+app.get("/", function (req, res) {
+  res.send("Server ON")
+})
 
-}).listen(port,() => {
-  console.log(`Server running at port `+ port);
-});
+app.post("/login", function (req, res) {
+  security.login(req, res)
+})
+
+app.get("/security", security.verifyJWT,(req, res) => {
+  res.send("security")
+})
 
 
 
+//app.use('/sistema', rotaSistema);
+//app.use('/user', rotaUsers);
+//app.use('/login', rotaLogin);
+//app.use('/data', rotaData);
+//app.use('/client',rotaClient)
 
-const jwt = require("jsonwebtoken");
-const chave = "OrganizaDevMiui#2021";
-
-exports.verifyJWT = (req, res, next) => {
-  const token = req.headers['x-access-token'];
-  if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
-  
-  jwt.verify(token,chave , function(err, decoded) {
-    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-    next();
-  });
-};
-
-exports.dataJWT = function(token) {
-  return jwt.verify(token, chave);
-};
+app.listen(process.env.PORT || 3000, () => console.log("Server ON"));
+module.exports = app;
--- security.js
+++ security.js
@@ -1,6 +1,42 @@
 const jwt = require("jsonwebtoken");
 const chave = "OrganizaDevMiui#2021";
 
+exports.login = (req, res) => {
+  
+  let validationError = false;
+  const { user, password } = req.body;
+
+  if(!user) {
+    res.status(400).send("user is not valid");
+    validationError = true;
+  }
+  if(!password) {
+    res.status(400).send("password is not valid");
+    validationError = true;
+  }
+  
+  const objUser = {
+    user: "victor",
+    password: "123"
+  }
+
+  if (user === objUser.user && password === objUser.password && !validationError){
+   jwt.sign(objUser, chave, (err, token) => {
+      if (err) {
+          res
+          .status(500)
+          .json({ mensagem: "Erro ao gerar o JWT" });
+          return;
+        }
+        res.send(token);
+        res.end();
+    });
+  } else {
+    res.status(401);
+    res.end();
+  }
+}
+
 exports.verifyJWT = (req, res, next) => {
   const token = req.headers['x-access-token'];
   if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
