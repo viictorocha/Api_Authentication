8ad9194c212bf7b395fd42a5e0c9989dcd4b93a6
10viictor39 <84255334+10viictor39@users.noreply.github.com>
2022-03-24 13:08:15 -0300

    validando rotas

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const security = require('./security.js')
 
-//const rotaSistema  = require('./routes/sistema');
-//const rotaUsers    = require('./routes/user');
-//const rotaLogin    = require('./routes/login');
-//const rotaData     = require('./routes/data');
-//const rotaClient   = require('./routes/client');
+const loginRouter = require('./routes/login');
 
 app.use(bodyParser.urlencoded({ extended: false })); //Aceita apenas dados simples
 app.use(bodyParser.json());
@@ -18,21 +13,7 @@ app.get("/", function (req, res) {
   res.send("Server ON")
 })
 
-app.post("/login", function (req, res) {
-  security.login(req, res)
-})
-
-app.get("/security", security.verifyJWT,(req, res) => {
-  res.send("security")
-})
-
-
-
-//app.use('/sistema', rotaSistema);
-//app.use('/user', rotaUsers);
-//app.use('/login', rotaLogin);
-//app.use('/data', rotaData);
-//app.use('/client',rotaClient)
+app.use('/login', loginRouter);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server ON"));
 module.exports = app;
